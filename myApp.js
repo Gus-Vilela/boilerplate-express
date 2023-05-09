66e8b5ee8cf36d9df958e1426a88208f4394d86b
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-09 13:57:00 +0000

    Update myApp.js and package-lock.json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+console.log("Hello World");
