8e62836bea837575f941002929b9cc85ae46d94f
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-09 14:18:36 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
+let string = "Hello Express";
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.send(string);
 });
 console.log("Hello World");
