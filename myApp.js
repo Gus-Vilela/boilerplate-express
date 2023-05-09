117ac8413c504ea9ebcd494c178c6e12e99ae9a7
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-09 14:54:37 +0000

    Update myApp.js and server.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 let express = require("express");
 let app = express();
 let string = "Hello Express";
+// app.get("/", (req, res) => {
+//   res.send(string);
+// });
+absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
-  res.send(string);
+  res.sendFile(absolutePath);
 });
 console.log("Hello World");
+module.exports = app;
