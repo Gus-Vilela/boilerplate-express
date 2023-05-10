399375a676b80ca6acf2e5e55b98e710a385e523
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-10 15:09:04 +0000

    Update myApp.js, add .env variables

--- myApp.js
+++ myApp.js
@@ -1,12 +1,29 @@
+require("dotenv").config();
+const { response } = require("express");
 let express = require("express");
 let app = express();
-let string = "Hello Express";
+// let string = "Hello Express";
 // app.get("/", (req, res) => {
 //   res.send(string);
 // });
+
+app.use("/public", express.static(__dirname + "/public"));
+
 absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
-console.log("Hello World");
+
+app.get("/json", (req, res) => {
+  let response = "";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
+  res.json({ message: response });
+});
+
+// console.log("Hello World");
+
 module.exports = app;
